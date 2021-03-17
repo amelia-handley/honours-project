560a573c33845a8a7ad3627b1c010cdab9461044
amelia-handley <amelia.handley7@gmail.com>
2021-03-17 18:37:12 +0000

    Visualisations working for both algorithms.

--- src/project.py
+++ src/project.py
@@ -1,22 +1,26 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
+import csv
+import re
+
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox as msg
-import csv
-import re
-import pyLDAvis
-import pyLDAvis.sklearn
 
-from sklearn.cluster import KMeans
 import nltk
 from nltk.stem import WordNetLemmatizer, PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords, wordnet
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.manifold import TSNE
+
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from numpy import concatenate
+import pyLDAvis
+import pyLDAvis.sklearn
 
 nltk.download('wordnet')
 nltk.download('stopwords')
@@ -97,9 +101,10 @@ class TwitterApplication:
             col_list = ["text"]
             array = []
             df = pd.read_csv(self.file_name, usecols=col_list)
-            stop_words = set(stopwords.words('english'))
-            # newStopWords = ('amp', 'rt')
-            # stopwords.update(newStopWords)
+            stopwords = nltk.corpus.stopwords.words('english')
+            newStopWords = ['amp', 'rt', 'http']
+            stopwords.append(newStopWords)
+
             lemmatizer = WordNetLemmatizer()
             ps = PorterStemmer()
 
@@ -113,42 +118,39 @@ class TwitterApplication:
                     
                     To remove URLs and non-alphanumeric characters
                     """
-
                     tweet = (re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "",
                                     tweet.lower()).split())
                     # clean_tweets.append(set(tweet).difference(stop_words))
-                    filtered_sentence = [w for w in tweet if not w in stop_words]
+                    filtered_sentence = [w for w in tweet if not w in stopwords]
                     array.append(filtered_sentence)
 
+                    # lemmatizer.lemmatize()
                     """
-                    #array = []
+                    array = []
                     for word in filtered_sentence:
                         test = lemmatizer.lemmatize(word, wordnet.VERB)
-                        stem_test = 
-                        nltk_tokens = nltk.word_tokenize(test)
-                        
-                    
-                    array.append(nltk_tokens)
-                    #print(array)"""
+                        stem = ps.stem(test)
+                        nltk_tokens = nltk.word_tokenize(stem)
+                    array.append(nltk_tokens) """
 
-
-            with open('../JSON-CSV-files/clean_csv.csv', 'w') as csvfile:
-                 writer = csv.writer(csvfile)
-                 writer.writerow(array)
+            # with open('../JSON-CSV-files/clean_csv.csv', 'w') as csvfile:
+            #   writer = csv.writer(csvfile)
+            #   writer.writerow(array)
 
             # Create a new dataframe of the cleaned dataset
-            #df['text'] = array
+            df['text'] = array
             # Add to file called clean_csv
-            clean_file = r"../JSON-CSV-files/clean_csv.csv"
-            # df.to_csv(clean_file, index=False)
+            clean_file = r"../JSON-CSV-files/clean_file_LDA.csv"
+            df.to_csv(clean_file, index=False)
             msg.showinfo('File', 'File has been cleaned. Ready to be converted.')
 
         except FileNotFoundError as e:
-             msg.showerror('Error opening file', e)
+            msg.showerror('Error opening file', e)
 
     """
         Using the Sklearn library to create the K-Means algorithm
     """
+
     def k_means(self):
         try:
             # https://github.com/Ashwanikumarkashyap/k-means-clustering-tweets-from-scratch/blob/master/main.py
@@ -158,33 +160,44 @@ class TwitterApplication:
                                                         filetypes=(('csv file', '*.csv'),
                                                                    ('csv file', '*.csv')))
 
-            #col_list = ["text"]
-            array = []
             df = pd.read_csv(self.file_name)
-            stop_words = set(stopwords.words('english'))
-            # newStopWords = ('amp', 'rt')
-            # stopwords.update(newStopWords)
-            lemmatizer = WordNetLemmatizer()
-            ps = PorterStemmer()
-
             vectorizer = TfidfVectorizer(stop_words='english')
             X = vectorizer.fit_transform(df)
-
-            true_k = 10  # Create 10 clusters
-            model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-            model.fit(X)
-
-            # https://pythonprogramminglanguage.com/kmeans-text-clustering/
+            n_clusters = 10  # Create 10 clusters
+            random_state = 1
+            clf = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=100, n_init=1)
+            data = clf.fit(X)
+            centroids = clf.cluster_centers_
+            everything = concatenate((X.todense(), centroids))
 
             print(f"Top terms per cluster: ")
-            order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+            order_centroids = data.cluster_centers_.argsort()[:, ::-1]
             terms = vectorizer.get_feature_names()
-            for i in range(true_k):
+            for i in range(n_clusters):
                 print("Cluster %d:" % i),
                 for ind in order_centroids[i, :10]:
                     print(' %s' % terms[ind])
                 print()
 
+            # https://stackoverflow.com/questions/43541187/how-can-i-plot-a-kmeans-text-clustering-result-with-matplotlib/45510082
+            tsne_init = 'pca'
+            tsne_perplexity = 20.0
+            tsne_early_exaggeration = 4.0
+            tsne_learning_rate = 1000
+            model = TSNE(n_components=2, random_state=random_state, init=tsne_init,
+                         perplexity=tsne_perplexity,
+                         early_exaggeration=tsne_early_exaggeration, learning_rate=tsne_learning_rate)
+
+            transformed_everything = model.fit_transform(everything)
+            print(transformed_everything)
+            plt.scatter(transformed_everything[:-n_clusters, 0], transformed_everything[:-n_clusters, 1], marker='x')
+            plt.scatter(transformed_everything[-n_clusters:, 0], transformed_everything[-n_clusters:, 1], marker='o')
+
+            plt.show()
+
+            # https://pythonprogramminglanguage.com/kmeans-text-clustering/
+
+            """
             # plt.subplots(figsize=(20,20))
             wordcloud = WordCloud(stopwords=stopwords,
                                   background_color='white',
@@ -192,7 +205,7 @@ class TwitterApplication:
                                   height=384
                                   ).generate(model)
             plt.imshow(wordcloud, interpolation='bilinear')
-            plt.axis('off')
+            plt.axis('off')"""
 
             plt.show()
 
@@ -202,6 +215,7 @@ class TwitterApplication:
     """
         Latent-Dirichlet Allocation using the Sklearn library
     """
+
     def lda(self):
         try:
             # https://ourcodingclub.github.io/tutorials/topic-modelling-python/
@@ -231,9 +245,9 @@ class TwitterApplication:
                 print("\n")"""
 
             # Visualise the topics
-            pyLDAvis.enable_notebook()
+            # pyLDAvis.enable_notebook()
 
-            visualisation = pyLDAvis.sklearn.prepare(LDA_output, doc_term_matrix, mds='tsne')
+            visualisation = pyLDAvis.sklearn.prepare(LDA_output, doc_term_matrix, count_vect, mds='tsne')
             # visualisation = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary)
             pyLDAvis.save_html(visualisation, 'LDA_Visualization.html')
 
